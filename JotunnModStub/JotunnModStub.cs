073590749bd2686843136b112f20422c851d8a73
Bryan Smith <mugenman1111@gmail.com>
2021-04-26 14:52:03 -0500

    idksomeenemything

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -45,44 +45,17 @@ namespace JotunnModStub
         private void LoadAssets()
         {
             Jotunn.Logger.LogInfo($"Embedded resources: {string.Join(",", Assembly.GetExecutingAssembly().GetManifestResourceNames())}");
-            embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("masterchef", Assembly.GetExecutingAssembly());
-            //insert prefabs here
-            turnipburgerfab = embeddedResourceBundle.LoadAsset<GameObject>("Assets/MasterChef/turnip_burger/turnipburger.prefab");
-
+            embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("enemy", Assembly.GetExecutingAssembly());
+            
 
         }
 
         private void CreateFood()
         {
-            var burger_prefab = embeddedResourceBundle.LoadAsset<GameObject>("turnipburger");
-            var burger = new CustomItem(burger_prefab, fixReference: false,
-                new ItemConfig
-                {
-                    Amount = 1,
-                    CraftingStation = "piece_cauldron",
-                    Requirements = new[]
-                    {
-                        new RequirementConfig { Item = "Honey", Amount = 1}
-                    }
-                });
-            ItemManager.Instance.AddItem(burger);
-        }
-
-        //can clone a prefab from game with this but it duplicates CreateFood() in a non working way
-        private void Foodrecipes()
-        {
-            CustomRecipe turnipburger = new CustomRecipe(new RecipeConfig()
-            {
-                Item = "turnipburger",
-                CraftingStation = "piece_cauldron",
-                Amount = 1,
-                Requirements = new[]
-                {
-                    new RequirementConfig {Item = "Honey", Amount = 1}
-                }
-            });
-            ItemManager.Instance.AddRecipe(turnipburger);
+            var burger_prefab = embeddedResourceBundle.LoadAsset<GameObject>("TestyWizard");
+            PrefabManager.Instance.AddPrefab(burger_prefab);
 
         }
+
     }
 }
